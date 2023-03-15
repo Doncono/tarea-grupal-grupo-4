0b95498a7126714178955e5a9e50f44877d1fa9d
Doncono <127454130+Doncono@users.noreply.github.com>
2023-03-14 19:40:19 -0600

    Add files via upload

---
+++ TAREAGRUPAL#4/src/tareagrupal/pkg4/Persona.java
@@ -0,0 +1,31 @@
+package Protect;
+
+
+public abstract class Persona {
+    private String especialidad;
+    public Persona(){
+        
+    }
+    // metodos set y get//
+    public void Establecerespecialidad(String especialidad){
+        this.especialidad=especialidad;
+    }
+    public String ObtenerEspecialidad(){
+        return this.especialidad;
+    }
+    
+    public void imprimirEspecialidad(){
+     System.out.println("Especialidad: "+especialidad);
+     
+     
+    }
+     // metodos abstractos//
+    abstract String getnombre();
+    abstract String getexperiencia();
+    abstract String direccion();
+    
+     
+    abstract String informacionpersona();
+    
+}
+
---
+++ TAREAGRUPAL#4/src/tareagrupal/pkg4/TAREAGRUPAL4.java
@@ -0,0 +1,55 @@
+package Protect;
+
+
+public class Deportista extends Persona {
+    //variable para encapsular
+ private int edad;
+ public String nombre;
+ public String direccion;
+ public String experiencia;
+   
+    
+    public  Deportista(int edad){
+        this.edad=edad;
+        Establecerespecialidad("Posicion"+nombre);
+    }
+    //encapsulamiento
+    
+    public void imprimiredad(){
+        System.out.println("La edad  es: " + edad);
+    
+    }
+    public void setNombre(String name){
+    this.nombre = name;
+    }
+    public void setexperiencia(String name){
+    this.experiencia=name;
+    }
+    public void setdireccion(String name){
+    this.direccion = name;
+    }
+    
+    
+    ///////abstraccion
+    
+   public String getnombre() {
+        return "nombre";
+    }
+
+    
+    public String getexperiencia() {
+        return "experiencia";
+    }
+
+    
+    public String direccion() {
+        return "direccion";
+    }
+
+    
+    public String informacionpersona() {
+        return "Futbolista";
+    }
+    
+    
+}
